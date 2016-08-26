55c425e8c104dd2084a561e21861354fd38d1331
wzhan366 <wzhan366@uwo.ca>
2016-08-25 21:32:41 -0400

    Add files via upload
    example for threading program

---
+++ thread.py
@@ -0,0 +1,31 @@
+from threading import Thread
+
+import urllib
+import re
+symbolfile = open("symbols.txt",'r')
+symbolslist = symbolfile.read().split('\n')
+
+
+def th(ur):
+	base = 'https://ca.finance.yahoo.com/q?s=' 
+	url = base + ur
+	regex = '<span id="yfs_l84_'+ ur.lower() +'">(.+?)</span>'
+	pattern = re.compile(regex)
+	htmltext = urllib.urlopen(url).read()
+	results = re.findall(pattern, htmltext)
+	print ur, results
+
+urls = ["http://www.businessinsider.com/", 
+		"http://nytimes.com",
+		"http://www.yahoo.com",
+		"http://cnn.com"]
+threadlist = []
+
+
+for u in symbolslist:
+	t = Thread(target=th, args=(u,))
+	t.start()
+	threadlist.append(t)
+
+for b in threadlist:
+	b.join()
